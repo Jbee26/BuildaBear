3dfa3f2cbdad0c2b85a8aa6a4c24d7663a20b71b
Jbee26 <jack_belyea26@milton.edu>
2023-11-06 15:17:11 -0500

    Initial commit

---
+++ src/Bear.java
@@ -0,0 +1,28 @@
+public class Bear {
+    public static void main(String[] args) {System.out.println("Hello world!"); }
+
+        public int limbs;
+        public String shape;
+        public String color;
+        public boolean fluff;
+        public int weight;
+
+
+        public void printInfo() {
+            System.out.println("This stuffed animal has " + limbs + " limbs");
+            System.out.println("This stuffed animal has a " + shape + " shape" );
+            System.out.println("This teddy is " + color);
+            if (fluff == true) {
+                System.out.println("VERY FLUFFY ");
+            } else {
+                System.out.println("Kina fluffy ");
+            }
+            System.out.println("This animal weighs " + weight + " pounds ");
+
+
+
+
+
+
+    }
+}
---
+++ src/BearHQ.java
@@ -0,0 +1,41 @@
+public class BearHQ {
+
+
+    public static void main(String[] args) { BearHQ myFactory = new BearHQ(); }
+
+
+
+
+
+
+    public BearHQ(){
+
+        Bear Octopus;
+        Octopus = new Bear();
+        Octopus.limbs = 4;
+        Octopus.shape = "circle";
+        Octopus.color = "purple";
+        Octopus.fluff = false;
+        Octopus.weight = 100;
+        Octopus.printInfo();
+
+
+        Bear Bear;
+        Bear = new Bear();
+        Bear.limbs = 8;
+        Bear.shape="bear";
+        Bear.color = "brown";
+        Bear.fluff = true;
+        Bear.weight = 600;
+        Bear.printInfo();
+
+
+
+
+
+
+
+    }
+
+
+        }
